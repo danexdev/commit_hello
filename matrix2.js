0849e03da0a38cfe8ced766a3b0cc723bb2480a3
Dmytro Rudenko <danex.dev@gmail.com>
2020-01-06 06:42:34 +0100

    GitHub Contribution Art

--- matrix2.js
+++ matrix2.js
@@ -10,6 +10,7 @@ const FILL_BACKGROUND = process.env.FILL_BACKGROUND === "true";
 const COMMIT_MESSAGE = process.env.COMMIT_MESSAGE;
 
 const SUNDAY_NUMBER = 0;
+const SATURDAY_NUMBER = 6;
 
 // Helper: Get the first Sunday of the year
 function getFirstSunday(year) {
@@ -20,41 +21,71 @@ function getFirstSunday(year) {
   return date;
 }
 
+function getLastSaturday(year) {
+  const date = new Date(`${year}-12-31`);
+  while (date.getDay() !== SATURDAY_NUMBER) {
+    date.setDate(date.getDate() - 1);
+  }
+  return date;
+}
+
 function generateCommitDates(year, text, fillBackground) {
   const firstSunday = getFirstSunday(year);
-  const totalDaysInYear = Math.ceil((new Date(`${year}-12-31`) - firstSunday + 1) / (24 * 60 * 60 * 1000));
-  const totalColumns = Math.ceil(totalDaysInYear / 7); // Weeks in the year
+  const lastSaturday = getLastSaturday(year);
+  const totalColumns = Math.ceil((lastSaturday - firstSunday + 1) / (7 * 24 * 60 * 60 * 1000));
   const rows = 7; // Sunday-Saturday
   const maxLength = Math.floor(totalColumns / 6); // Fit 6 columns per character
   const truncatedText = text.slice(0, maxLength);
+  console.log("totalDaysInYear", {totalColumns, rows, maxLength, truncatedText});
 
   // Use a Set to store unique dates
-  const commitDatesSet = new Set();
-  const matrixStartRow = 2; // Start rendering from the 2nd row (align text vertically)
+  // const commitDatesSet = new Set();
+  const matrixStartRow = 1; // Start rendering from the 2nd row (align text vertically)
+
+
+//   const matrix = Array.from({ length: rows }, (_, rowIndex) =>
+//   Array.from({ length: totalColumns }, (_, colIndex) => {
+//     const date = new Date(firstSunday);
+//     date.setDate(firstSunday.getDate() + rowIndex * 7 + colIndex);
+//     return date;
+//   })
+// );
+const matrix = Array.from({ length: rows }, (_, rowIndex) =>
+  Array.from({ length: totalColumns }, (_, colIndex) => {
+    const date = new Date(firstSunday);
+    // Calculate the correct date for each cell in the matrix
+    date.setDate(firstSunday.getDate() + colIndex * 7 + rowIndex);
+    return date;
+  })
+);
+
+const commitDates = []
+  // console.log("matrix", matrix);
 
   truncatedText.split("").forEach((char, charIndex) => {
     const charMap = CHAR_MAP[char] || CHAR_MAP[" "];
     const charStartCol = charIndex * 6; // 6 columns per character
 
-    charMap.forEach((charRow, rowIndex) => {
-      const row = rowIndex + matrixStartRow; // apply offset
-      for (let colIndex = 0; colIndex < charRow.length; colIndex++) {
-        const isPixelFilled = charRow[colIndex] === "#";
-        const colPosition = charStartCol + colIndex;
+    // console.log("charMap", {charMap,char, charIndex, charStartCol});
 
-        console.log(`Char: ${char}, row: ${row}, col: ${colPosition}, filled: ${isPixelFilled}`);
+charMap.forEach((charRow, rowIndex) => {
+  const row = rowIndex + matrixStartRow; // Apply vertical offset for rendering
+  for (let colIndex = 0; colIndex < charRow.length; colIndex++) {
+    const isPixelFilled = charRow[colIndex] === "#";
+    const colPosition = charStartCol + colIndex;
 
-        if (isPixelFilled && colPosition <= totalColumns && row <= rows) {
-          const commitDayOffset = colPosition * 7 + row + (colPosition >= 12 ? 1: 0); // Calculate exact day offset
-          const commitDate = new Date(firstSunday);
-          commitDate.setDate(firstSunday.getDate() + commitDayOffset);
-          commitDatesSet.add(commitDate.toISOString().split("T")[0]);
-        }
-      }
-    });
+    // Ensure we don't exceed matrix boundaries
+    if (isPixelFilled && row < rows && colPosition < totalColumns) {
+      const date = matrix[row][colPosition]; // Get date from the matrix
+      commitDates.push(date.toISOString().split("T")[0]); // Add date in YYYY-MM-DD format
+    }
+  }
+
+  console.log("commitDates", commitDates.sort());
+});
   });
 
-  return [...commitDatesSet];
+  return commitDates.sort();
 }
 
 // Generate commit dates based on .env configuration
